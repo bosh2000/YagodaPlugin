77feae9762cbd22d18383bab2c7b7dcd734accb2
bosh <kupel@inbox.ru>
2018-06-15 07:50:47 +1000

    Init commit

---
+++ Resto.Front.Api.YagodaPlugin/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reactive.Disposables;
+using Resto.Front.Api.V6;
+
+namespace Resto.Front.Api.YagodaPlugin
+{
+    class YagodaPlugin : IFrontPlugin
+    {
+        private readonly CompositeDisposable subscriptions;
+        
+        public void Dispose()
+        {
+            subscriptions?.Dispose();
+        }
+    }
+}
